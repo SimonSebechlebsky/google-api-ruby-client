54f16a683164b6917097a6e96b60c48404a83a48
Simon <sajmon@Simons-MacBook-Pro.local>
2024-03-10 23:03:59 +0100

    fix(core): Require Pathname for checking download destination

--- google-apis-core/lib/google/apis/core/download.rb
+++ google-apis-core/lib/google/apis/core/download.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'pathname'
 require 'google/apis/core/api_command'
 require 'google/apis/errors'
 require 'addressable/uri'
